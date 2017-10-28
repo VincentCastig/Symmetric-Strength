59b00bb4529fa6dc5acad0321d1e9d92a0c21e3e
VincentCastig <vincent.castig@gmail.com>
2017-10-28 17:33:45 -0500

    Changed localhost back to heroku

--- public/services/currentService.js
+++ public/services/currentService.js
@@ -3,7 +3,7 @@ angular.module("myApp").service("currentService", function($http) {
     this.getCurrentData = function() {
         return $http({
             method: "GET",
-            url: 'http://localhost:3000/api/exercises'
+            url: 'http://vintruv.herokuapp.com/api/exercises'
         }).then(function(response) {
             response.data.numRecords = 10;
             response.data.page = 1;
@@ -272,7 +272,7 @@ angular.module("myApp").service("currentService", function($http) {
     this.getLastCurrent = function() {
         return $http({
             method: "GET",
-            url: 'http://localhost:3000/api/lastexercise/'
+            url: 'http://vintruv.herokuapp.com/api/lastexercise/'
         }).then(function(response) {
         return response.data;
         })
@@ -281,7 +281,7 @@ angular.module("myApp").service("currentService", function($http) {
     this.getLastFiveCurrent = function() {
         return $http({
             method: "GET",
-            url: 'http://localhost:3000/api/exercises'
+            url: 'http://vintruv.herokuapp.com/api/exercises'
         }).then(function(response) {
           const weightLiftData = {
             deadlift: [],
@@ -314,7 +314,7 @@ angular.module("myApp").service("currentService", function($http) {
     this.addCurrentData = function(frontsquat, backsquat, pendlayrow, deadlift, shoulderpress, inclinebench, benchpress, bicepcurl, tricepcurl, deadliftreps, pendlayrowreps, backsquatreps, frontsquatreps, benchpressreps, inclinebenchreps, shoulderpressreps, bicepcurlreps, tricepcurlreps) {
         return $http({
             method: "POST",
-            url: 'http://localhost:3000/api/exercise/',
+            url: 'http://vintruv.herokuapp.com/api/exercise/',
             data: {
               "frontsquat" : frontsquat, 
               "backsquat" : backsquat, 
